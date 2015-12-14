c7b3d3ffe72b0f0c43dc04dced3e9f279c8a60f4
veselints <veselintsvetanov@gmail.com>
2015-12-15 00:11:25 +0200

    Implemented basic initial view

---
+++ YamAndRateApp/YamAndRateApp/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using YamAndRateApp.Views;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace YamAndRateApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void GoToSearchResultsBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void GoToLogInBtn(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(LogInView));
+        }
+
+        public void GoToNearbyRestaurantsBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void GoToAddNewRestaurantBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void GoToAllRestaurantsBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
