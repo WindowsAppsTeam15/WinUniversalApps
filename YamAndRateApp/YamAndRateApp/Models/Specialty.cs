0ebcedb1e768b0374614df1fce320a6b92f97c2a
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2015-12-17 14:33:26 +0200

    Added CategoryType.cs and Specialty.cs

---
+++ YamAndRateApp/YamAndRateApp/Models/Specialty.cs
@@ -0,0 +1,15 @@
+﻿namespace YamAndRateApp.Models
+{
+    using Parse;
+
+    [ParseClassName("specialty")]
+    public class Specialty : ParseObject
+    {
+        [ParseFieldName("name")]
+        public string Name
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty<string>(value); }
+        }
+    }
+}
