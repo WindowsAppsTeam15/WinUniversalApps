c9a08f0842cdd0a03d46699bd41bf5cbd60d9747
veselints <veselintsvetanov@gmail.com>
2015-12-20 22:29:57 +0200

    Username displays in the header

---
+++ YamAndRateApp/YamAndRateApp/ViewModels/HeaderViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Parse;
+
+namespace YamAndRateApp.ViewModels
+{
+    public class HeaderViewModel : ViewModelBase
+    {
+        private string currentUsername;
+
+        public HeaderViewModel()
+        {
+            var user = ParseUser.CurrentUser;
+            if (user != null )
+            {
+                this.CurrentUsername = user.Username;
+            }
+        }
+
+        public string CurrentUsername {
+            get
+            {
+                return this.currentUsername;
+            }
+            set
+            {
+                if (this.currentUsername != value)
+                {
+                    this.currentUsername = value;
+                    this.NotifyPropertyChanged("CurrentUsername");
+                }
+            }
+        }
+    }
+}
